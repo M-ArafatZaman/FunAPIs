29a25f5330f2009c95a15c9eae18658b56fc1e27
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-11-28 20:48:21 +0600

    barplot test - added test for a proper dataset which returns an image

--- tests/test_barplot.py
+++ tests/test_barplot.py
@@ -8,8 +8,11 @@ def test_barplot(client):
     expect_400 = client.get("/api/barplot/")
     assert expect_400.status_code == 400
 
-
     # Test with a foul data
     expect_500 = client.get("/api/barplot/?data=hello%20world")
     assert expect_500.status_code == 500
     
+    # Test a proper request
+    expect_success = client.get("/api/barplot/?data=1,2,3")
+    assert expect_success.status_code == 200
+    assert expect_success.headers["Content-Type"] == "image/png"
