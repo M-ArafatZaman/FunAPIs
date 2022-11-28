d87e7fed26c1d7664a3e32500fb90c8c4a3022c3
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-11-28 20:36:11 +0600

    barplot test - test for 500 request

--- tests/test_barplot.py
+++ tests/test_barplot.py
@@ -6,6 +6,10 @@ def test_barplot(client):
 
     # Test without any data
     expect_400 = client.get("/api/barplot/")
-
     assert expect_400.status_code == 400
 
+
+    # Test with a foul data
+    expect_500 = client.get("/api/barplot/?data=hello%20world")
+    assert expect_500.status_code == 500
+    
